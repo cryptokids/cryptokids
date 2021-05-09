97bd5d28bf6721bce4c7076b8ac45547ca846cdb
Alexey Gapchenko <deus@ExMachina.local>
2021-05-09 14:17:19 +0300

    Changed text

--- src/App.js
+++ src/App.js
@@ -41,16 +41,14 @@ export default function App() {
   if (!window.walletConnection.isSignedIn()) {
     return (
       <main>
-        <h1>Welcome to NEAR!</h1>
+        <h1>Welcome to CryptoKids!</h1>
         <p>
-          To make use of the NEAR blockchain, you need to sign in. The button
+          CryptoKids is a non-profit project built on NEAR Protocol blockchain.
+          You need to sign in to start using the app. The button
           below will sign you in using NEAR Wallet.
         </p>
         <p>
-          By default, when your app runs in "development" mode, it connects
-          to a test network ("testnet") wallet. This works just like the main
-          network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
-          convertible to other currencies – they're just for testing!
+          The App is under development right now.
         </p>
         <p>
           Go ahead and click the button below to try it out:
