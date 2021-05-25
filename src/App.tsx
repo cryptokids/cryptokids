a5e3c8404c9fa757aec5e6a036d1331e4cae4b35
makadaw <makadaw@gmail.com>
2021-05-25 23:07:36 +0200

    Integrate react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-
-import './global.css'
-import Home from './components/Home'
 import { useRecoilValueLoadable } from 'recoil'
+import { useRecoilValue } from 'recoil'
+import { ToastContainer } from 'react-toastify'
+import { Redirect, Route, Switch } from 'react-router'
+import { HashRouter } from 'react-router-dom'
 import { nearState } from './state/near'
 import { AuthProvider } from './components/AuthProvider'
-import { useRecoilValue } from 'recoil'
 import { isLoggedInState } from './state/authentication'
+
+import './global.css'
+import 'react-toastify/dist/ReactToastify.css'
+import Home from './components/Home'
 import Welcome from './components/Welcome'
 
 const App: React.FC = () => {
@@ -15,14 +19,23 @@ const App: React.FC = () => {
   const isLoggedIn = useRecoilValue(isLoggedInState)
 
   return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      {loadNear.state == 'hasValue' && loadNear.contents && (
-        <AuthProvider>
-          {!isLoggedIn && <Welcome />}
-          {isLoggedIn && <Home />}
-        </AuthProvider>
-      )}
-    </React.Suspense>
+    <React.Fragment>
+      <ToastContainer position="bottom-right" autoClose={5000} />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        {loadNear.state == 'hasValue' && loadNear.contents && (
+          <AuthProvider>
+            <HashRouter>
+              <Switch>
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/">
+                  {isLoggedIn ? <Redirect to="/home" /> : <Welcome />}
+                </Route>
+              </Switch>
+            </HashRouter>
+          </AuthProvider>
+        )}
+      </React.Suspense>
+    </React.Fragment>
   )
 }
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,23 @@
 import React, { useCallback, useEffect } from 'react'
-import { toast, ToastContainer } from 'react-toastify'
+import { toast } from 'react-toastify'
 import { useRecoilValue, useRecoilState } from 'recoil'
 import { userState } from '../state/authentication'
 import { greetingState } from '../state/greeting'
 import { IContract, nearState } from '../state/near'
-import { SignOutLink } from './AuthProvider'
 import Footer from './Footer'
 import GreetingForm from './GreetingForm'
 import { networkId } from '../foundation/near-utils'
-import 'react-toastify/dist/ReactToastify.css'
+import Layout from './Layout/Index'
 
 const Home: React.FC = () => {
   const userInfo = useRecoilValue(userState)
   const { contract } = useRecoilValue(nearState)
-  if (!userInfo || !contract) return null
+
   const [greeting, setGreeting] = useRecoilState(greetingState)
 
   const updateGreeting = useCallback(
     async (newGreeting) => {
+      if (!userInfo || !contract) return
       if (
         await contract.setGreeting({
           accountId: userInfo.accountId,
@@ -31,6 +31,7 @@ const Home: React.FC = () => {
   )
 
   useEffect(() => {
+    if (!userInfo || !contract) return
     async function loadGreeting(contract: IContract) {
       if (!userInfo) return
       const greeting = await contract.getGreeting({
@@ -42,53 +43,45 @@ const Home: React.FC = () => {
   }, [setGreeting, userInfo])
 
   return (
-    <React.Fragment>
-      <SignOutLink>
-        <button className="link" style={{ float: 'right' }}>
-          Sign out
-        </button>
-      </SignOutLink>
-      <main>
-        <h1>
-          <label
-            htmlFor="greeting"
-            style={{
-              color: 'var(--secondary)',
-              borderBottom: '2px solid var(--secondary)',
-            }}
-          >
-            {greeting}
-          </label>
-          {
-            ' ' /* React trims whitespace around tags; insert literal space character when needed */
-          }
-          {userInfo.accountId}
-        </h1>
-        <GreetingForm
-          greeting={greeting}
-          onSave={async (newGreeting) => {
-            await updateGreeting(newGreeting)
-            // show Notification
-            toast(
-              toastMessage({
-                accountId: userInfo.accountId,
-                contractId: contract.contractId,
-              })
-            )
-            // setShowNotification(true)
-
-            // remove Notification again after css animation completes
-            // this allows it to be shown again next time the form is submitted
-            setTimeout(() => {
-              // setShowNotification(false)
-            }, 11000)
-            return true
+    <Layout auth={true}>
+      <h1>
+        <label
+          htmlFor="greeting"
+          style={{
+            color: 'var(--secondary)',
+            borderBottom: '2px solid var(--secondary)',
           }}
-        ></GreetingForm>
-        <Footer />
-      </main>
-      <ToastContainer position="bottom-right" />
-    </React.Fragment>
+        >
+          {greeting}
+        </label>
+        {
+          ' ' /* React trims whitespace around tags; insert literal space character when needed */
+        }
+        {userInfo?.accountId}
+      </h1>
+      <GreetingForm
+        greeting={greeting}
+        onSave={async (newGreeting) => {
+          await updateGreeting(newGreeting)
+          // show Notification
+          toast(
+            toastMessage({
+              accountId: userInfo!.accountId,
+              contractId: contract!.contractId,
+            })
+          )
+          // setShowNotification(true)
+
+          // remove Notification again after css animation completes
+          // this allows it to be shown again next time the form is submitted
+          setTimeout(() => {
+            // setShowNotification(false)
+          }, 11000)
+          return true
+        }}
+      ></GreetingForm>
+      <Footer />
+    </Layout>
   )
 }
 
