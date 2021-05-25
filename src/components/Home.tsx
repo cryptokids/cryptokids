098857db41aa98b41458843fcebdb5150dd9b157
makadaw <makadaw@gmail.com>
2021-05-25 21:19:12 +0200

    Add tostify for notifications

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
-import { Contract } from 'near-api-js'
 import React, { useCallback, useEffect } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
 import { useRecoilValue, useRecoilState } from 'recoil'
 import { userState } from '../state/authentication'
 import { greetingState } from '../state/greeting'
@@ -7,14 +7,14 @@ import { IContract, nearState } from '../state/near'
 import { SignOutLink } from './AuthProvider'
 import Footer from './Footer'
 import GreetingForm from './GreetingForm'
-import Notification from './Notification'
+import { networkId } from '../foundation/near-utils'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Home: React.FC = () => {
   const userInfo = useRecoilValue(userState)
   const { contract } = useRecoilValue(nearState)
   if (!userInfo || !contract) return null
   const [greeting, setGreeting] = useRecoilState(greetingState)
-  const [showNotification, setShowNotification] = React.useState(false)
 
   const updateGreeting = useCallback(
     async (newGreeting) => {
@@ -69,24 +69,50 @@ const Home: React.FC = () => {
           onSave={async (newGreeting) => {
             await updateGreeting(newGreeting)
             // show Notification
-            setShowNotification(true)
+            toast(
+              toastMessage({
+                accountId: userInfo.accountId,
+                contractId: contract.contractId,
+              })
+            )
+            // setShowNotification(true)
 
             // remove Notification again after css animation completes
             // this allows it to be shown again next time the form is submitted
             setTimeout(() => {
-              setShowNotification(false)
+              // setShowNotification(false)
             }, 11000)
             return true
           }}
         ></GreetingForm>
         <Footer />
       </main>
-      {showNotification && (
-        <Notification
-          accountId={userInfo.accountId}
-          contractId={contract.contractId}
-        />
-      )}
+      <ToastContainer position="bottom-right" />
+    </React.Fragment>
+  )
+}
+
+const toastMessage = ({
+  accountId,
+  contractId,
+}: {
+  accountId: string
+  contractId: string
+}) => {
+  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`
+  return (
+    <React.Fragment>
+      🦄{' '}
+      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${accountId}`}>
+        {accountId}
+      </a>
+      {
+        ' ' /* React trims whitespace around tags; insert literal space character when needed */
+      }
+      called method: 'set_greeting' in contract:{' '}
+      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${contractId}`}>
+        {contractId}
+      </a>
     </React.Fragment>
   )
 }
