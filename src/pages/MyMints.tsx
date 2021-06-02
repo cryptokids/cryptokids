46faefe58c34d3f5f11ce99a1548b4c413474974
makadaw <makadaw@gmail.com>
2021-06-02 18:58:00 +0200

    Display MyMints

--- src/pages/MyMints.tsx
+++ src/pages/MyMints.tsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRecoilValue } from 'recoil'
+import { IAccount, IWallet, nearState } from '../state/near'
+import Card from '../components/Card'
+
+const ThingCard: React.FC<{ thing: any }> = ({ thing }) => {
+  return (
+    <Card
+      username={'me'}
+      title={thing.title}
+      price={{ fraction: 1, token: 'NEAR' }}
+      url={typeof thing.media === 'string' ? thing.media : thing.media.data.uri}
+    ></Card>
+  )
+}
 
 const MyMints: React.FC = () => {
-  return <p>My mints</p>
+  const { mintbase, account } = useRecoilValue(nearState)
+  const [things, setThings] = useState([] as {}[])
+
+  useEffect(() => {
+    async function loadMarketplace(wallet: IWallet, account: IAccount) {
+      const { data, error } = await wallet.api!.fetchAccount(account.accountId)
+      if (error) {
+        console.error(error)
+        return
+      }
+      const { token } = data
+      Promise.all(
+        token.map(async (t: { thing: { id: string }; id: string }) => {
+          const { data } = await wallet.api!.fetchThingMetadata(t.thing.id)
+          return data ? data : null
+        })
+      ).then((result) => {
+        console.log(result)
+        setThings(result as any[])
+      })
+
+      console.log(token)
+    }
+    if (mintbase && account) {
+      loadMarketplace(mintbase, account)
+    }
+  }, [setThings])
+
+  return (
+    <div className="grid place-items-center sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 p-5">
+      {things.map((thing, idx) => {
+        return <ThingCard key={`thing_${idx}`} thing={thing} />
+      })}
+    </div>
+  )
 }
 
 export default MyMints
