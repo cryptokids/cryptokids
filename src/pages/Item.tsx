be8ca05d612a56cd6bacdc35715acf74bf59ed5d
Alexey Gapchenko <deusson@gmail.com>
2021-06-08 10:51:51 +0300

    Revert styles for item view

--- src/pages/Item.tsx
+++ src/pages/Item.tsx
@@ -31,7 +31,7 @@ const Item: React.FC = () => {
 
   // TODO: Handle error state
   return (
-    <div className="place-items-center gap-3 p-5">
+    <div className="grid place-items-center sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-3 p-5">
       {metadata.state === 'hasValue' && metadata.contents && (
         <>
           <Card
